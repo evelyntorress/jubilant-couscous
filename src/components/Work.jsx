45e65e2e181ce19228cde4df06655cadfab44737
Evelyn Torres <evelyn@Evelyns-MacBook-Air.local>
2022-08-03 11:51:59 -0700

    adding the last project

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import onClickNews from "../assets/onclick-news.jpeg";
 import expressMovies from "../assets/express-movies.jpeg";
+import eccommerceImage from "../assets/image-1.png"
 
 function Work() {
   return (
@@ -63,8 +64,32 @@ function Work() {
               </div>
             </div>
           </div>
-
           {/* Project 3 */}
+          <div>
+            <p className="text-xl font-semibold py-4">E-commerce Website</p>
+            <img src={eccommerceImage} alt="profile" width="400" height="400" />
+            <div>
+              <div className="pt-4 text-center flex">
+                <a
+                  href="https://damp-earth-50504.herokuapp.com/"
+                  target={"blank"}
+                >
+                  <button className="text-black border-2 border-gray-600 px-4 py-1 my-3 flex items-center hover:bg-sky-500/60 hover:border-gray-500 mr-5">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/andresparraarze/project-3"
+                  target={"blank"}
+                >
+                  <button className="text-black border-2 border-gray-600 px-5 py-1 my-3 flex items-center hover:bg-sky-500/60 hover:border-gray-500">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        {/* Project 1 */}
           <div>
             <p className="text-xl font-semibold py-4">OnClick News</p>
             <img src={onClickNews} alt="profile" width="400" height="400" />
@@ -89,7 +114,7 @@ function Work() {
               </div>
             </div>
           </div>
-          {/* Project 4 */}
+          {/* Project 2 */}
           <div>
             <p className="text-xl font-semibold py-4">Express Movies</p>
             <img src={expressMovies} alt="profile" width="400" height="400" />
@@ -114,7 +139,32 @@ function Work() {
               </div>
             </div>
           </div>
-        </div>
+        {/* Project 3 */}
+          <div>
+            <p className="text-xl font-semibold py-4">E-commerce Website</p>
+            <img src={eccommerceImage} alt="profile" width="400" height="400" />
+            <div>
+              <div className="pt-4 text-center flex">
+                <a
+                  href="https://damp-earth-50504.herokuapp.com/"
+                  target={"blank"}
+                >
+                  <button className="text-black border-2 border-gray-600 px-4 py-1 my-3 flex items-center hover:bg-sky-500/60 hover:border-gray-500 mr-5">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/andresparraarze/project-3"
+                  target={"blank"}
+                >
+                  <button className="text-black border-2 border-gray-600 px-5 py-1 my-3 flex items-center hover:bg-sky-500/60 hover:border-gray-500">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div> 
       </div>
     </div>
   );
